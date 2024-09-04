d92e16531a355670fb96c05d3ff50fc4655e70c0
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-09-04 21:53:57 +0300

    create method write file

--- src/v1_0_1/service/write/FileWriteService.java
+++ src/v1_0_1/service/write/FileWriteService.java
@@ -1,4 +1,18 @@
 package v1_0_1.service.write;
 
+import v1_0_1.utils.Constants;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileWriteService {
+    public String writeFile(String fileName, String content) {
+        try (FileOutputStream fOut = new FileOutputStream(Constants.PATH_SAVE_FILE + fileName)) {
+            byte[] contentBytes = content.getBytes();
+            fOut.write(contentBytes);
+        }catch (IOException e) {
+           return e.getMessage();
+        }
+        return "Success";
+    }
 }
