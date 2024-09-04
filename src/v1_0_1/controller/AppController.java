5ebc36a52077a2189f1ccf90818d8230650832a3
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-09-05 00:16:46 +0300

    modified

--- src/v1_0_1/controller/AppController.java
+++ src/v1_0_1/controller/AppController.java
@@ -1,10 +1,8 @@
 package v1_0_1.controller;
 
 import v1_0_1.service.create.FileCreateService;
-import v1_0_1.utils.Constants;
 import v1_0_1.utils.InputData;
-
-import java.io.File;
+import v1_0_1.view.ShowMenu;
 
 public class AppController {
     FileCreateService fileCreateService;
@@ -12,8 +10,23 @@ public class AppController {
 
     public void run() {
         fileCreateService = new FileCreateService();
+        new ShowMenu().showMenu();
         inputData = new InputData();
-//        fileCreateService.createFile("file");
-        Path root = new File(Constants.PATH_SAVE_FILE)
+        choiceMenu(Integer.parseInt(inputData.input()));
+    }
+
+    private void choiceMenu(int choice) {
+        switch (choice) {
+            case 1:
+                System.out.print("Input name package: ");
+                if (fileCreateService.createFile(inputData.input())) {
+                    System.out.println("Created file successfully.");
+                } else {
+                    System.out.println("Failed to create file.");
+                }
+                run();
+                break;
+        }
+
     }
 }
--- src/v1_0_1/service/create/FileCreateService.java
+++ src/v1_0_1/service/create/FileCreateService.java
@@ -5,7 +5,7 @@ import v1_0_1.utils.Constants;
 import java.io.File;
 
 public class FileCreateService {
-    public void createFile(String fileName) {
-        new File(Constants.PATH_CREATE_FILE + fileName).mkdir();
+    public boolean createFile(String fileName) {
+      return new File(Constants.PATH_CREATE_FILE + fileName).mkdir();
     }
 }
