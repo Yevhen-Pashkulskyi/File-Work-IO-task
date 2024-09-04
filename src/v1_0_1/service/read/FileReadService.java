0454773b142f04250b5059713219ac46ca15fa5d
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-09-04 22:16:42 +0300

    created method read service

--- src/v1_0_1/service/read/FileReadService.java
+++ src/v1_0_1/service/read/FileReadService.java
@@ -1,4 +1,25 @@
 package v1_0_1.service.read;
 
+import v1_0_1.utils.Constants;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class FileReadService {
+    public String readFile(String fileName) {
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(
+                             new FileInputStream(Constants.PATH_SAVE_FILE + fileName)))) {
+            String textMsg;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((textMsg = reader.readLine()) != null) {
+                stringBuilder.append(textMsg).append("\n");
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
 }
