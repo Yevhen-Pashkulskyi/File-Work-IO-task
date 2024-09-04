32d2f6a233255d63ebf44f55352cdc71a923b06d
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-09-04 23:40:20 +0300

    modified

--- src/v1_0_1/service/create/FileCreateService.java
+++ src/v1_0_1/service/create/FileCreateService.java
@@ -5,7 +5,7 @@ import v1_0_1.utils.Constants;
 import java.io.File;
 
 public class FileCreateService {
-    public boolean createFile(String fileName) {
-        return new File(Constants.PATH_CREATE_FILE + fileName).mkdir();
+    public void createFile(String fileName) {
+        new File(Constants.PATH_CREATE_FILE + fileName).mkdir();
     }
 }
