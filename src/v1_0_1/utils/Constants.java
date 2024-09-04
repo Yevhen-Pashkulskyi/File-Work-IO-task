f795d92085601d4d8a3ae39a6349e15fbbbd1462
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-09-05 00:00:55 +0300

    modified

--- src/v1_0_1/utils/Constants.java
+++ src/v1_0_1/utils/Constants.java
@@ -2,5 +2,6 @@ package v1_0_1.utils;
 
 public class Constants {
     public static final String PATH_SAVE_FILE = "files/";
-    public static final String PATH_CREATE_FILE = PATH_SAVE_FILE;
+    public static final String PATH_CREATE_FILE =
+            "/Volumes/trancend/learn/it/Java/Idea/Hillel/java/javaPro/projects/File-Work-IO-task/";
 }
--- src/v1_0_1/view/ShowMenu.java
+++ src/v1_0_1/view/ShowMenu.java
@@ -1,12 +1,12 @@
 package v1_0_1.view;
 
 public class ShowMenu {
-    public String showMenu() {
-        System.out.println("Select a task!\n");
-        return "1) Create package.\n" +
+    public void showMenu() {
+        System.out.print("1) Create package.\n" +
                 "2) Create a file with the extension \"txt\".\n" +
                 "3) Write to a file.\n" +
                 "4) Read from a file.\n" +
-                "0) Close program.";
+                "0) Close program.\n");
+        System.out.print("Select a task: ");
     }
 }
